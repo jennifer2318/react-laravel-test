5598c6dd02e2e75cbdfe25e5f8416490f1e7a78f
pguczol23 <38167698+pguczol23@users.noreply.github.com>
2020-12-06 02:09:41 +0300

    migrations

---
+++ database/migrations/2020_12_05_221833_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->string('description');
+            $table->dateTime('created_at')->nullable(false);
+            $table->double('price')->nullable(false);
+            $table->integer('count')->nullable(false);
+            $table->bigInteger('external_id')->nullable(false)->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
